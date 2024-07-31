011ab8c7b5551a14390c33db95729850d622cd09
aaglis <aglissilvafon@gmail.com>
2024-07-31 16:12:51 -0300

    feat(signup): with swiper and more fields

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../shared/components/default-login-layout/default-login-layout.component';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
@@ -11,44 +11,86 @@ import Swal from 'sweetalert2';
   standalone: true,
   imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, RouterLink, InputLayoutComponent],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class SignupComponent {
+export class SignupComponent implements AfterViewInit {
   router = inject(Router)
   formBuilder = inject(FormBuilder)
   register = inject(RegisterService)
 
+  @ViewChild('swiper', { static: false }) swiper?: ElementRef;
+  currentSlide = 1;
+
+  ngAfterViewInit() {
+    if (this.swiper) {
+      const swiper = this.swiper.nativeElement.swiper;
+
+      swiper.on('slideChange', () => {
+        this.currentSlide = swiper.realIndex + 1;
+      });
+    }
+  }
+
   registerForm = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.minLength(8)]],
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    name: [``],
+    alias: [``, [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
+    cpf: ['', [Validators.required, Validators.minLength(11)]],
+    dateOfBirth: ['', [Validators.required]],
+    phone: ['', [Validators.required, Validators.minLength(11)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength]],
+    cep: ['', [Validators.minLength(8)]],
+    streetName: ['', [Validators.minLength(5)]],
+    city: ['', [Validators.minLength(3)]],
+    residenceNumber: ['', [Validators.minLength(1)]],
   })
 
+
+
   submit() {
-    if(this.registerForm.valid) {
-      this.register.registerUser(this.registerForm.value).subscribe({
-        next: (response) => {
-          Swal.fire({
-            title: "Login feito com sucesso!",
-            text: "Você será redirecionado para a página de login.",
-            icon: "success",
-            showCloseButton: true,
-            showCancelButton: true,
-            confirmButtonText: 'Fazer login',
-            cancelButtonText: "Cancelar"
-          }).then((result) => {
-            if(result.isConfirmed) {
-              this.router.navigate(['/login'])
-            }
-          })
-          console.log(response)
-        },
-        error: (error) => {
-          console.error(error)
-        }
-      })
-    } else {
-      console.log('Form is invalid')
+    console.log(this.registerForm.value)
+    // if(this.registerForm.valid) {
+    //   this.register.registerUser(this.registerForm.value).subscribe({
+    //     next: (response) => {
+    //       Swal.fire({
+    //         title: "Login feito com sucesso!",
+    //         text: "Você será redirecionado para a página de login.",
+    //         icon: "success",
+    //         showCloseButton: true,
+    //         showCancelButton: true,
+    //         confirmButtonText: 'Fazer login',
+    //         cancelButtonText: "Cancelar"
+    //       }).then((result) => {
+    //         if(result.isConfirmed) {
+    //           this.router.navigate(['/login'])
+    //         }
+    //       })
+    //       console.log(response)
+    //     },
+    //     error: (error) => {
+    //       console.error(error)
+    //     }
+    //   })
+    // } else {
+    //   console.log('Form is invalid')
+    // }
+  }
+
+  goToNextSlide() {
+    if (this.swiper) {
+      const swiper = this.swiper.nativeElement.swiper;
+      swiper.slideNext();
+    }
+  }
+
+  goToPreviousSlide() {
+    if (this.swiper) {
+      const swiper = this.swiper.nativeElement.swiper;
+      swiper.slidePrev();
     }
   }
 
